cd768ae6f6ba10b3aeb19b72ed8ef8855ec06e50
Hkly <hannahkyiu@gmail.com>
2013-12-05 13:08:07 -0800

    Create script.js

---
+++ colorme/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+	for (var i = 1; i <= 2000; i++){
+		$('#color_area').append('<div class = "small_box" />');
+	}
+	
+	var colorList = ["red", "orange", "yellow", "green", "blue", "purple", "white"];
+	var currColor;
+
+	$("#erase").click(function(){
+		currColor = "white";
+	});
+	
+	$("#red").click(function(){
+		currColor = "red";
+	});
+	
+	$("#orange").click(function(){
+		currColor = "orange";
+	});
+	
+	$("#yellow").click(function(){
+		currColor = "yellow";
+	});
+	
+	$("#green").click(function(){
+		currColor = "green";
+	});
+	
+	$("#blue").click(function(){
+		currColor = "blue";
+	});
+	
+	$("#purple").click(function(){
+		currColor = "purple";
+	});
+	
+	$(".small_box").mouseover(
+		function(){
+			$( this ).removeClass().addClass("small_box " + currColor);
+
+	});
+		
+});
