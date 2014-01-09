15e5fddfeeda118fdf0e417be99d1ddb6cbc18e0
Hannah <hannahkyiu@gmail.com>
2014-01-08 22:57:01 -0800

    condense code, add click and drag to draw

--- colorme/js/script.js
+++ colorme/js/script.js
@@ -10,37 +10,26 @@ $(document).ready(function(){
 		currColor = "white";
 	});
 	
-	$("#red").click(function(){
-		currColor = "red";
+	$(".swatch").click(function(){
+		currColor = $(this).text();
 	});
-	
-	$("#orange").click(function(){
-		currColor = "orange";
-	});
-	
-	$("#yellow").click(function(){
-		currColor = "yellow";
-	});
-	
-	$("#green").click(function(){
-		currColor = "green";
-	});
-	
-	$("#blue").click(function(){
-		currColor = "blue";
-	});
-	
-	$("#purple").click(function(){
-		currColor = "purple";
-	});
-    
-	$(".small_box").mouseover(function(){
-		$( this ).removeClass().addClass("small_box " + currColor);
-		});
-  
-  $("#clear").click(function(){
-    $(".small_box").removeClass().addClass("small_box");
-	currColor = "white";
+
+	$("#clear").click(function(){
+		$(".small_box").css("background-color", "white");
+		currColor = "white";
 	});	
+
+	
+	$(".small_box").mousedown(function(){
+		$(".small_box").on("mouseover", this, function(){
+			$( this ).css("background-color", currColor);
+		});
+	});
+
+	$(".small_box").mouseup(function(){
+		$(".small_box").off("mouseover");
+	});
+
+
 		
 });
